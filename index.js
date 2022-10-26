11bf8d8942feee183cb3412a3e8b040e5e7369d6
maruf <55664274+maruf11604@users.noreply.github.com>
2022-10-26 20:10:59 +0600

    new app created

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port=process.env.PORT || 5000;
 
 
 const topics= require('./data/topic.json');
+const categories=require('./data/categories.json');
 
 
 
@@ -16,6 +17,10 @@ app.get('/',(req,res)=>{
 
 app.get('/topics',(req,res)=>{
     res.send(topics);
+})
+
+app.get('/category',(req,res)=>{
+    res.send(categories)
 })
 
 app.listen(port,()=>{
