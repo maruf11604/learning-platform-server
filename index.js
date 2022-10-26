ff182308049b449fa86d94d89a6575e9b34f2217
maruf <55664274+maruf11604@users.noreply.github.com>
2022-10-26 20:08:32 +0600

    added category data on data

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express= require('express');
 const app=express();
+const cors=require('cors');
 const port=process.env.PORT || 5000;
 
+
+
 const topics= require('./data/topic.json');
+
+
+
+app.use(cors());
 app.get('/',(req,res)=>{
     res.send('api running on port')
 })
